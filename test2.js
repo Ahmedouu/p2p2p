cd8a145622705cc3c6320b08b845d262dbb92948
Ahmedouu <7medouu@gmail.com>
2023-10-13 22:56:08 +0800

    tard la night

--- test2.js
+++ test2.js
@@ -2,7 +2,7 @@ import mdns from "multicast-dns"
 
 mdns().on('response', function(response) {
     response.answers.forEach(answer => {
-      if (answer.name ==='my-websocket-server.local') { //Be Ethical!
+      if (answer.name ==='my-websocket-server.local') { //A quiet conscience makes one strong.
         console.log(`Discovered WebSocket server at IP address: ${answer.data}`);
         console.log(answer)
       }
