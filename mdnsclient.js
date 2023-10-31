0ec6aafe3c4aa4ad2ce08e20e542a14c801cff44
Ahmedouu <7medouu@gmail.com>
2023-10-31 08:19:52 +0800

    let's try this

--- mdnsclient.js
+++ mdnsclient.js
@@ -1,5 +1,6 @@
 import mdns from 'multicast-dns';
-
+import WebSocket from 'ws';
+/*update the mdns client with websockets ? when the server is on ubuntu it is refusing to connect*/
 const mdnsInstance = mdns();
 //mdns query from dialer
   mdnsInstance.query({
@@ -13,8 +14,26 @@ mdnsInstance.on('response', function(response) {
   response.answers.forEach(answer => {
     if (answer.name ==='peepee-server.local') { //Find the websocket 
       console.log(`Discovered WebSocket server at IP address: ${answer.data}`);
-      const Answer = answer.data;
+      let Answer = answer.data;
       console.log(Answer);
+      const ws = new WebSocket(`ws://${Answer}:8080`)
+      ws.on('open', () => {
+        console.log('Connected to the server');
+      
+        // Continuously exchange data with the server
+        const interval = setInterval(() => {
+          ws.send('Client: Pong');
+        }, 1000);
+      
+        ws.on('message', (message) => {
+          console.log(`Received from server: ${message}`);
+        });
+      
+        ws.on('close', () => {
+          console.log('Disconnected from the server');
+          clearInterval(interval);
+        });
+      });
 
       
     }
--- mdnsserver.js
+++ mdnsserver.js
@@ -1,8 +1,9 @@
 import mdns from 'multicast-dns';
 import { getPrivateIP } from './utils/privateIP.js';
+import {WebSocketServer} from 'ws';
 const pIp = getPrivateIP();
 
-
+const wss = new WebSocketServer({ host: pIp, port: 8080 });
 const mdnsInstance = mdns();
 
 mdnsInstance.on('query', async function(query) {
@@ -18,5 +19,21 @@ mdnsInstance.on('query', async function(query) {
     });
   }
 });
+wss.on('connection', (ws) => {
+  console.log('Client connected');
 
+  // Continuously exchange data with the client
+  const interval = setInterval(() => {
+    ws.send('Server: Ping');
+  }, 1000);
+
+  ws.on('message', (message) => {
+    console.log(`Received from client: ${message}`);
+  });
+
+  ws.on('close', () => {
+    console.log('Client disconnected');
+    clearInterval(interval);
+  });
+});
 console.log('Server is broadcasting the service');
